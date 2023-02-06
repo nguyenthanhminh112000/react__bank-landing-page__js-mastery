82cad58d194d70ea4ff1a6f3dc0b814ad941e8de
Minh Nguyen <minh_nguyen@epam.com>
2023-02-06 13:14:06 +0700

    commit for push

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className=" bg-red-600 w-full py-6 flex justify-between items-center navbar">
+      Navbar
+      <div>a</div>
+    </nav>
+  );
+};
+
+export default Navbar;
